f7ae91b35411ccde5f143538fc338dd7802c6ca5
razvanpaduraru <razvan.paduraru@stud.acs.upb.ro>
2020-04-27 14:01:00 +0300

    First Fe Commit

---
+++ Google Maps Api/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", (event) => {
+  console.log("Service worker has been installed.");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Service has fetched a resource.");
+});
